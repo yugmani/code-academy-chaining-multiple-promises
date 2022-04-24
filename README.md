923caa33e4889f0cb41940b7cd79f286a11f18ef
yugmani <yugmani@yahoo.com>
2022-04-24 16:31:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-b2u9fj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-b2u9fj)
---
+++ index.js
@@ -0,0 +1,16 @@
+// Import stylesheets
+import './style.css';
+
+// Chaining Multiple Promises
+// *****************************************
+
+//Here’s a simple promise chain in code:
+/*
+firstPromiseFunction()
+.then((firstResolveVal)=>{
+  return secondPromiseFunction(firstResolveVal);
+})
+.then((secondResolveVal)=>{
+  console.log(secondResolveVal);
+});
+*/
